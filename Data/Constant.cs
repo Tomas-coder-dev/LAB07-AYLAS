8397a972a6e7532ec59b66c66c9d1d745e0498fa
htorrico <htorrico@truenorthcorporation.com>
2025-10-02 18:43:36 -0500

    Add project files.

---
+++ Data/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public  static class Constant
+    {
+        public static string _connectionString = "Server=localhost;Database=StoreDB;User Id=sa;Password=your_password;";
+    }
+}
---
+++ Data/DProduct.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.Data.SqlClient;
+namespace Data
+{
+    public class DProduct
+    {
+
+ 
+        public void Create(Product product)
+        {
+           
+        }
+        public List<Product> Read()
+        {
+            var products = new List<Product>();
+
+            using (SqlConnection conn = new SqlConnection(Constant._connectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_list_products", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var product = new Product
+                        {
+                            ProductID = reader.GetInt32(reader.GetOrdinal("product_id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                            Price = reader.GetDecimal(reader.GetOrdinal("price")),
+                            Stock = reader.GetInt32(reader.GetOrdinal("stock")),
+                            Active = reader.GetBoolean(reader.GetOrdinal("active"))
+                        };
+
+                        products.Add(product);
+                    }
+                }
+            }
+
+            return products;
+        }
+        public void Update(Product product)
+        {
+
+        }
+        public void Delete(Product product)
+        {
+
+        }
+
+    }
+}
